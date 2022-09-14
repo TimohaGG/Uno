bb03d187f2309215a8909956db4ea33623b060e2
Timoha <timoha22.a11@gmail.com>
2022-09-14 11:52:22 +0200

    Adding class CardsCharacteristics

---
+++ Core/CardsCharacteristics/WildCardsInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno_V2.Core.CardsCharacteristics
+{
+    internal class WildCardsInfo : ICharacterisable
+    {
+        public ConsoleColor[] ColorVariants { get; }
+        public string[] CardsSuits { get; }
+        public int FirstCardNum { get; set; } = 52;
+        public Card.CardType Type { get; } = Card.CardType.Wild;
+        public const int ColorAmount = 2;
+        public const int SuitAmount = 2;
+
+        public WildCardsInfo()
+        {
+            ColorVariants = new ConsoleColor[ColorAmount]
+            {
+               ConsoleColor.Gray,
+               ConsoleColor.Gray
+            };
+            CardsSuits = new string[SuitAmount]
+            {
+                "ChC","+ 2"
+            };
+        }
+    }
+}
--- Core/DeckCreator.cs
+++ Core/DeckCreator.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using Uno_V2.Core.CardsCharacteristics;
 
 namespace Uno_V2.Core
 {
@@ -11,57 +9,42 @@ namespace Uno_V2.Core
         //----------fileds----------
         private Card[] deck;
         private int ColorNumber = 4;
-        private ConsoleColor[] ColorVariants = new ConsoleColor[4]
-        {
-            ConsoleColor.Blue,
-            ConsoleColor.Green,
-            ConsoleColor.Red,
-            ConsoleColor.Yellow
-        };
-        private ConsoleColor[] WildColor = new ConsoleColor[2]
-        {
-            ConsoleColor.Gray,
-            ConsoleColor.Gray
-        };
-        private string[] CardsSuits = new string[10]
-        {
-            " 0 "," 1 "," 2 "," 3 "," 4 "," 5 ", " 6 "," 7 "," 8 "," 9 "
-        };
-        private string[] SpecialCardsSuits = new string[3]
-        {
-            "+ 1","ChD"," S "
-        };
-        private string[] WildCardSuits = new string[2]
-        {
-             "ChC","+ 2"
-        };
 
-        //----------constructor----------
+        CardsCharasteristics charasteristics;
+        
         public DeckCreator(Card[] deck)
         {
             this.deck = deck;
+            charasteristics = new CardsCharasteristics();
         }
 
         //----------methods----------
         public Card[] CreateDeck()
         {
-            //dColoredCards();
-            AddCards(CardsSuits,ColorVariants,0, Card.CardType.Regular);
-            AddCards(SpecialCardsSuits, ColorVariants, 40, Card.CardType.Special);
-            AddCards(WildCardSuits, WildColor, 52, Card.CardType.Wild);
+          
+            AddCards(charasteristics.RegularInf);
+            AddCards(charasteristics.SpecInf);
+            AddCards(charasteristics.WildInf);
             
             Reshuffle();
             return deck;
         }
 
         
-        private void AddCards(string[] suits, ConsoleColor []colors, int CardNum, Card.CardType Type)
+        private void AddCards(ICharacterisable chrctr)
         {
-            for (int colorN = 0; colorN < colors.Length; colorN++)
+            int ColorsAmount = chrctr.ColorVariants.Length;
+            int SuitsAmount = chrctr.CardsSuits.Length;
+            string[] CardsSuits = chrctr.CardsSuits;
+            var Colors = chrctr.ColorVariants;
+            var Type = chrctr.Type;
+
+            for (int colorN = 0; colorN < ColorsAmount; colorN++)
             {
-                for (int suitN = 0; suitN < suits.Length;)
+                for (int suitN = 0; suitN < SuitsAmount; suitN++)
                 {
-                    deck[CardNum++] = new Card(suits[suitN++], colors[colorN], Type);
+                    deck[chrctr.FirstCardNum++] = 
+                        new Card(CardsSuits[suitN], Colors[colorN], Type);
                 }
             }
         }
